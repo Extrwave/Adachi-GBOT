b1dd35ddcab3a354298b6b918ec8dac7aee8d283
Extrwave <exwave@foxmail.com>
2022-07-26 16:31:53 +0800

    fix: modify sponsor image fix: modify panel plugin name

--- src/plugins/@help/achieves/sponsor.ts
+++ src/plugins/@help/achieves/sponsor.ts
@@ -7,21 +7,17 @@ CreateTime: 2022/7/26
 
 export async function main( i: InputParameter ): Promise<void> {
 	
-	const userID = i.messageData.msg.author.id;
 	const content = i.messageData.msg.content;
-	const qqPay = "http://cdn.ethreal.cn/img/1658803956743-1658803958.png";
-	const wechatPay = "http://cdn.ethreal.cn/img/WeChatPay-1654016432.png";
-	const aliPay = "http://cdn.ethreal.cn/img/Alipay-1654016352.png";
-	const sponsorImage = "http://cdn.ethreal.cn/img/sponsor-1658802365.jpg";
+	const qqPay = "http://cdn.ethreal.cn/img/QqSponsor-1658823605.jpg";
+	const wechatPay = "http://cdn.ethreal.cn/img/WechatSponsor-1658823617.jpg";
+	const aliPay = "http://cdn.ethreal.cn/img/AliPaySponsor-1658823707.jpg";
 	
 	if ( /qq/.test( content ) ) {
 		await i.sendMessage( { content: `感谢您的支持 ~ `, image: qqPay } );
-	} else if ( /wechat/.test( content ) ) {
-		await i.sendMessage( { content: `感谢您的支持 ~ `, image: wechatPay } );
-	} else if ( /alipay/.test( content ) ) {
+	} else if ( /zfb/.test( content ) ) {
 		await i.sendMessage( { content: `感谢您的支持 ~ `, image: aliPay } );
 	} else {
-		await i.sendMessage( { content: `感谢您的支持 ~ `, image: sponsorImage } );
+		await i.sendMessage( { content: `感谢您的支持 ~ `, image: wechatPay } );
 	}
 	return;
 }
