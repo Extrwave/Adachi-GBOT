787406b2f0f69104994095913097bbbd17074da9
Extrwave <exwave@foxmail.com>
2022-10-06 10:45:19 +0800

    - fix: Some config changes does not take effect - style: Optimize expansion items, allow only one item be opened - feat: Add page for modify plugin config in webconsole

--- src/modules/management/auth.ts
+++ src/modules/management/auth.ts
@@ -2,47 +2,44 @@ import BotConfig from "@modules/config";
 import Database from "@modules/database";
 
 export enum AuthLevel {
-    Banned,
-    User,
-    Manager,
-    Master
+	Banned,
+	User,
+	Manager,
+	Master
 }
 
 interface AuthorizationMethod {
-    set( userID: string, level: AuthLevel ): Promise<void>;
-
-    get( userID: string ): Promise<AuthLevel>;
-
-    check( userID: string, limit: AuthLevel ): Promise<boolean>;
+	set( userID: string, level: AuthLevel ): Promise<void>;
+	
+	get( userID: string ): Promise<AuthLevel>;
+	
+	check( userID: string, limit: AuthLevel ): Promise<boolean>;
 }
 
 export default class Authorization implements AuthorizationMethod {
-    private readonly master: string;
-    private readonly redis: Database;
-
-    constructor( config: BotConfig, redis: Database ) {
-        this.master = config.master;
-        this.redis = redis;
-    }
-
-    private static dbKey( userID: string ): string {
-        return `adachi.auth-level-${ userID }`;
-    }
-
-    public async set( userID: string, level: AuthLevel ): Promise<void> {
-        await this.redis.setString(Authorization.dbKey(userID), level);
-    }
-
-    public async get( userID: string ): Promise<AuthLevel> {
-        if ( userID === this.master ) {
-            return AuthLevel.Master;
-        }
-        const auth: string | null = await this.redis.getString(Authorization.dbKey(userID));
-        return !auth ? AuthLevel.User : parseInt(auth);
-    }
-
-    public async check( userID: string, limit: AuthLevel ): Promise<boolean> {
-        const auth: AuthLevel = await this.get(userID);
-        return auth >= limit;
-    }
+	private readonly master: string;
+	private readonly redis: Database;
+	private static dbKey: string = `adachi.auth-level`;
+	
+	constructor( config: BotConfig, redis: Database ) {
+		this.master = config.master;
+		this.redis = redis;
+	}
+	
+	public async set( userID: string, level: AuthLevel ): Promise<void> {
+		await this.redis.setHashField( Authorization.dbKey, userID, level );
+	}
+	
+	public async get( userID: string ): Promise<AuthLevel> {
+		if ( userID === this.master ) {
+			return AuthLevel.Master;
+		}
+		const auth: string | null = await this.redis.getHashField( Authorization.dbKey, userID );
+		return !auth ? AuthLevel.User : parseInt( auth );
+	}
+	
+	public async check( userID: string, limit: AuthLevel ): Promise<boolean> {
+		const auth: AuthLevel = await this.get( userID );
+		return auth >= limit;
+	}
 }
--- src/web-console/components/spread-form-item/index.js
+++ src/web-console/components/spread-form-item/index.js
@@ -2,40 +2,39 @@ const template = `<form-item ref="spreadRef" class="spread-form-item" :class="{ 
 	<span
 		v-if="!open"
 		:class="{ password: showBtnStyle }"
-		class="spread-show-text"
-	>
-		{{ spreadShowText }}
-	</span>
+		class="spread-show-text">{{ spreadShowText }}</span>
 	<template v-else>
-		<el-input-number
-			v-if="type === 'number'"
-			ref="inputRef"
-			v-model="formValue"
-			:min="0"
-			:controls="false"
-			:placeholder="placeholder"
-			:disabled="disabled"
-			@keyup.enter="saveValue"
-		/>
-		<Tags
-			v-else-if="type === 'list'"
-			v-model="formValue"
-			:disabled="disabled"
-			:limit="1"
-		/>
-		<el-input
-			v-else
-			ref="inputRef"
-			v-model="formValue"
-			:type="type === 'textarea' ? 'textarea' : 'test'"
-			:rows="4"
-			:placeholder="placeholder"
-			:disabled="disabled"
-			:show-password="type === 'password'"
-			@keyup.enter="type === 'textarea' ? '' : saveValue()"
-			clearable
-		/>
-		<span class="spread-warn-msg" v-if="showErrMsg">{{ verifyMsg }}</span>
+		<div class="input-content">
+			<el-input-number
+				v-if="type === 'number'"
+				ref="inputRef"
+				v-model="formValue"
+				:min="0"
+				:controls="false"
+				:placeholder="placeholder"
+				:disabled="disabled"
+				@keyup.enter="saveValue"
+			/>
+			<Tags
+				v-else-if="type === 'list'"
+				v-model="formValue"
+				:disabled="disabled"
+				:limit="1"
+			/>
+			<el-input
+				v-else
+				ref="inputRef"
+				v-model="formValue"
+				:type="type === 'textarea' ? 'textarea' : 'test'"
+				:rows="4"
+				:placeholder="placeholder"
+				:disabled="disabled"
+				:show-password="type === 'password'"
+				@keyup.enter="type === 'textarea' ? '' : saveValue()"
+				clearable
+			/>
+			<span class="spread-warn-msg" v-if="showErrMsg">{{ verifyMsg }}</span>
+		</div>
 		<div class="btn-list">
 			<el-button type="primary" size="small" @click.stop="saveValue">确认</el-button>
 			<el-button size="small" @click.stop="packUpItem">取消</el-button>
@@ -46,66 +45,72 @@ const template = `<form-item ref="spreadRef" class="spread-form-item" :class="{ 
 import FormItem from "../form-item/index.js";
 import Tags from "../../components/tags/index.js";
 
-const { defineComponent, watch, reactive, ref, toRefs, nextTick, computed } = Vue;
+const { defineComponent, getCurrentInstance, watch, reactive, ref, toRefs, nextTick, computed } = Vue;
 
 export default defineComponent( {
-	name: "SpreadFormItem",
-	template,
-	components: {
-		FormItem,
-		Tags
-	},
-	emits: [ "update:modelValue", "change", "open", "close" ],
-	props: {
+	name: "SpreadFormItem", template, components: {
+		FormItem, Tags
+	}, emits: [ "update:modelValue", "change", "open", "close" ], props: {
 		modelValue: {
 			default: ""
 		},
-		label: {
-			type: String,
+		/* 当前展开的组件id */
+		activeSpread: {
 			default: ""
+		},
+		label: {
+			type: String, default: ""
 		},
 		placeholder: {
-			type: String,
-			default: ""
+			type: String, default: ""
 		},
 		type: {
-			default: "normal",
-			validator( value ) {
+			default: "normal", validator( value ) {
 				return [ "normal", "number", "password", "list", "textarea" ].includes( value );
 			}
 		},
 		desc: {
-			type: String,
-			default: ""
+			type: String, default: ""
 		},
-		/* 正则校验 */
+		/* 行数，仅当 type 为 textarea 时有效 */
+		rows: {
+			type: Number,
+			default: 6
+		},
+		/* 当 type 为 password 时，该配置无效 */
+		hideContent: {
+			type: Boolean,
+			default: false
+		},
+		/* 校验，可为返回值为 Boolean 的方法或正则字符串 */
 		verifyReg: {
-			type: String,
 			default: ""
 		},
 		/* 校验错误提示文字 */
 		verifyMsg: {
-			type: String,
-			default: "请检查填写内容"
-		},
-		disabled: {
-			type: Boolean,
-			default: false
+			type: String, default: "请检查填写内容"
+		}, disabled: {
+			type: Boolean, default: false
 		}
-	},
-	setup( props, { emit } ) {
+	}, setup( props, { emit } ) {
 		const state = reactive( {
-			open: false,
-			showErrMsg: false,
-			formValue: ""
+			open: false, showErrMsg: false, formValue: ""
 		} );
 		
 		const inputRef = ref( null );
 		const spreadRef = ref( null );
+		const uid = getCurrentInstance().uid;
 		
 		watch( () => props.modelValue, value => {
 			state.formValue = value;
 		}, { immediate: true } );
+		
+		/* 当前展开项切换时，若非本组件，收起 */
+		watch( () => props.activeSpread, value => {
+			if ( value !== uid ) {
+				packUpItem();
+			}
+		} );
 		
 		watch( () => state.open, value => {
 			resetData();
@@ -116,21 +121,17 @@ export default defineComponent( {
 		
 		/* 是否在未开启时展示按钮效果 */
 		const showBtnStyle = computed( () => {
-			return props.type === "password" || state.formValue.length === 0
+			return props.hideContent || props.type === "password" || state.formValue.length === 0
 		} );
 		
 		const spreadShowText = computed( () => {
-			return showBtnStyle.value
-				? "点击设置"
-				: state.formValue instanceof Array
-					? state.formValue.join( "、" )
-					: state.formValue
+			return showBtnStyle.value ? "点击设置" : state.formValue instanceof Array ? state.formValue.join( "、" ) : state.formValue
 		} );
 		
 		/* 点击展开项 */
 		function spreadItem() {
 			if ( state.open || props.disabled ) return;
-			emit( "open" );
+			emit( "open", uid );
 			state.open = true;
 			nextTick( () => {
 				inputRef.value.focus();
@@ -145,10 +146,20 @@ export default defineComponent( {
 		
 		/* 保存数据 */
 		function saveValue() {
-			const reg = props.verifyReg ? new RegExp( `^${ props.verifyReg }$` ) : null;
-			if ( reg && !reg.test( state.formValue ) ) {
-				state.showErrMsg = true;
-				return;
+			const verify = props.verifyReg;
+			if ( verify ) {
+				if ( typeof verify === "function" ) {
+					if ( !verify( state.formValue ) ) {
+						state.showErrMsg = true;
+						return;
+					}
+				} else {
+					const reg = new RegExp( `^${verify}$` );
+					if ( reg && !reg.test( state.formValue ) ) {
+						state.showErrMsg = true;
+						return;
+					}
+				}
 			}
 			if ( state.formValue !== props.modelValue ) {
 				emit( "update:modelValue", state.formValue );
@@ -164,14 +175,7 @@ export default defineComponent( {
 		}
 		
 		return {
-			...toRefs( state ),
-			inputRef,
-			spreadRef,
-			showBtnStyle,
-			spreadShowText,
-			spreadItem,
-			packUpItem,
-			saveValue
+			...toRefs( state ), inputRef, spreadRef, showBtnStyle, spreadShowText, spreadItem, packUpItem, saveValue
 		}
 	}
 } )
