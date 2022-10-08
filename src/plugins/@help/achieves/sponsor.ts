27c3cc0de29a891e8e64a79c009d2bf3f50dd69c
Extrwave <exwave@foxmail.com>
2022-10-08 20:07:52 +0800

    fix: add 爱发电支持

--- src/plugins/@help/achieves/sponsor.ts
+++ src/plugins/@help/achieves/sponsor.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import { v4 } from 'uuid'
 import { InputParameter } from "@modules/command";
 
 /**
@@ -7,17 +9,9 @@ CreateTime: 2022/7/26
 
 export async function main( i: InputParameter ): Promise<void> {
 	
-	const content = i.messageData.msg.content;
-	const qqPay = "http://cdn.ethreal.cn/img/QqSponsor-1658823605.jpg";
-	const wechatPay = "http://cdn.ethreal.cn/img/WechatSponsor-1658823617.jpg";
-	const aliPay = "http://cdn.ethreal.cn/img/AliPaySponsor-1658823707.jpg";
-	
-	if ( /qq/.test( content ) ) {
-		await i.sendMessage( { content: `感谢您的支持 ~ `, image: qqPay } );
-	} else if ( /zfb/.test( content ) ) {
-		await i.sendMessage( { content: `感谢您的支持 ~ `, image: aliPay } );
-	} else {
-		await i.sendMessage( { content: `感谢您的支持 ~ `, image: wechatPay } );
-	}
+	await i.sendMessage( {
+		content: `感谢您的支持 ~ `,
+		image: "http://cdn.ethreal.cn/img/1665227901326-1665227902.png"
+	} );
 	return;
 }
